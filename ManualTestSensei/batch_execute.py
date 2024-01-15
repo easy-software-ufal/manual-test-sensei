73e1a39f038b851fde0fcea9dfc858059baffa8c
naelsondouglas <naelson17@gmail.com>
2024-01-15 19:51:35 -0300

    Fix

--- ManualTestSensei/batch_execute.py
+++ ManualTestSensei/batch_execute.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import ubuntu_data
 import time
+
 # from matchers.eager_step import EagerStep
 
 from matchers_facade import  MatchersFacade
--- ManualTestSensei/plot_graphs.py
+++ ManualTestSensei/plot_graphs.py
@@ -1,4 +1,5 @@
 from transformation.transformation_data import SMELL_NAMES
+from matplotlib import pyplot as plt
 import seaborn as sns
 
 if __name__ == '__main__':
@@ -8,3 +9,4 @@ if __name__ == '__main__':
             values = [float(value) for value in f.read().split('\n') if len(value) > 0]
         data[smell] = values
     sns.violinplot(data=data, fill=False)
+    plt.show()
